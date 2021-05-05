d7e1e4cc2ce77b4923cd4f1a410f934d3caddadb
Ziyun Li <me@ziyunli.dev>
2021-05-04 22:06:02 -0400

    add formatter

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject p-p-p-pokerface "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [iloveponies.tests/p-p-p-pokerface "0.2.0-SNAPSHOT"]]
-  :profiles {:dev {:plugins [[lein-midje "3.2.1"]]}})
+  :profiles {:dev {:plugins [
+                             [lein-midje "3.2.1"]
+                             [lein-cljfmt "0.7.0"]]}})
